af119183eeefb497e6d0c5926adc37330bf27da3
Piotr Jakubowski <pjakubowski220@gmail.com>
2024-10-13 15:02:07 +0200

    update- dataBase added to Vercel

--- server/index.js
+++ server/index.js
@@ -6,6 +6,17 @@ const connectToMongoDB = require('./mongoConnection');
 // Połącz się z MongoDB
 connectToMongoDB();
 
+
+app.get("/players", async (req, res) => {
+    try {
+        const players = await Player.find();
+        res.status(200).json(players);
+    } catch (error) {
+        console.error("Error fetching players:", error);
+        res.status(500).json({ message: "Error fetching players", error });
+    }
+});
+
 app.use("/", (req, res) => {
     res.send("Server is running");
 });
