43cb0f399f91ea1c1f863a02d00467c10f0d9335
Piotr Jakubowski <pjakubowski220@gmail.com>
2024-10-13 13:24:39 +0200

    update

--- server/mongoConnection.js
+++ server/mongoConnection.js
@@ -2,14 +2,11 @@ const mongoose = require('mongoose');
 
 const connectToMongoDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGODB_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
+        await mongoose.connect(process.env.MONGODB_URI);
         console.log("Połączono z MongoDB");
     } catch (error) {
         console.error("Błąd połączenia z MongoDB:", error);
-        process.exit(1); // Zakończ proces jeśli nie uda się połączyć
+        process.exit(1); // Zakończ proces, jeśli nie uda się połączyć
     }
 };
 
